0e5fe0d868e79a9112184a8a5e36eaefe82b0d42
monarchxr <raunakjais2131@gmail.com>
2025-08-15 23:40:31 +0530

    just a basic setup for main

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,16 @@
 from parser import parse_resume
+import json
+
+resume_file = "" #put path of resume file here for now
+
+parsed_data = parse_resume(resume_file)
+
+#save parsed data to json file
+
+with open("output.json", "w", encoding="utf-8") as f:
+    json.dump(parsed_data, f, indent=2)
+
+# display parsed results
+print("Resume data: ")
+for key,value in parsed_data.items():
+    print(f"{key.capitalize()}: {value}")
