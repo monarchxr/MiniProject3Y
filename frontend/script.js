78598769c2c6d67290816441bddb07f90e8c085d
monarchxr <raunakjais2131@gmail.com>
2025-08-18 16:51:24 +0530

    trying to connect some

--- frontend/script.js
+++ frontend/script.js
@@ -3,3 +3,9 @@ const btn = document.getElementById("submit_button");
 btn.addEventListener('click', ()=>{
     alert('Successfully submitted!');
 });
+
+document.getElementById("resume_form").addEventListener("submit", async function(e){
+    e.preventDefault();
+    //this is to stop default form submission
+})
+
