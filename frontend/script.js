0a16358476c974435261405ac502ce2004876828
monarchxr <raunakjais2131@gmail.com>
2025-08-20 21:46:07 +0530

    experimenting with js a bit, will update more later

--- frontend/script.js
+++ frontend/script.js
@@ -1,11 +1,19 @@
+document.getElementById("resume_form").addEventListener("submit", async function(e){
+    e.preventDefault();
+    //this is to stop default form submission
+
+
+    const resume = document.getElementById("resume").files[0];
+    const jobRole = document.getElementById("job_role").value;
+
+    if(!resume.files.length){
+        alert("Please upload a resume before submitting.");
+        return
+    }
+})
+
 const btn = document.getElementById("submit_button");
 
 btn.addEventListener('click', ()=>{
     alert('Successfully submitted!');
 });
-
-document.getElementById("resume_form").addEventListener("submit", async function(e){
-    e.preventDefault();
-    //this is to stop default form submission
-})
-
