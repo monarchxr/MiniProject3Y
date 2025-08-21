b6a642d6e43b34183389db03b1096faea96e02f5
monarchxr <raunakjais2131@gmail.com>
2025-08-21 12:27:27 +0530

    idk reset

---
+++ backend/starter.py
@@ -0,0 +1,18 @@
+from parser import parse_resume
+import json
+
+def process_resume(resume_file_path):
+
+
+    parsed_data = parse_resume(resume_file_path)
+
+    #save parsed data to json file
+
+    with open("output.json", "w", encoding="utf-8") as f:
+        json.dump(parsed_data, f, indent=2)
+
+    # display parsed results
+    print("Resume data: ")
+    
+    for key,value in parsed_data.items():
+        print(f"{key.capitalize()}: {value}")
